df7854649efc7ed460807915c5486bfc2c643227
aranzac <alu0100889680@ull.edu.es>
2022-12-29 20:01:08 +0000

    First commit

---
+++ src/test/java/com/spring/acc/skyapp/skyapp/SkyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.acc.skyapp.skyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
