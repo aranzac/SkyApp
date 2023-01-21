6213fc86b7f9b8322ba3f628b23ae9621b9da4bd
aranzac <alu0100889680@ull.edu.es>
2023-01-21 13:42:49 +0000

    Info endpoint

--- src/main/java/com/spring/acc/skyapp/skyapp/controller/MainController.java
+++ src/main/java/com/spring/acc/skyapp/skyapp/controller/MainController.java
@@ -1,8 +1,18 @@
 package com.spring.acc.skyapp.skyapp.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/sky")
 public class MainController {
-    
+
+    @GetMapping("/info")
+    public ResponseEntity<String> getInfo() {
+        return new ResponseEntity<>(String.format("Sending data"), HttpStatus.OK);
+    }
+
 }
